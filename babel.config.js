69fbce895b76a00d238225d0a6a438a5575b3e9c
Osk <oscar.martin@alu.ceacfp.es>
2026-05-08 01:53:22 +0200

    NoteFlow: Motor Metro estabilizado. Listo para produccion web.

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-worklets/plugin', // El motor ahora se llama así
+      'react-native-reanimated/plugin',
     ],
   };
 };
